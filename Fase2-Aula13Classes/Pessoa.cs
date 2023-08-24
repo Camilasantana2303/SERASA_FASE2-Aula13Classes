a35b05852cc64256a01529226a542afc1f02fb19
camila.scarneiro1 <camila.scarneiro1@CAR0588302W10-1>
2023-08-24 13:57:18 -0300

    Adicionar arquivos de projeto.

---
+++ Fase2-Aula13Classes/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase2_Aula13Classes
+{
+    internal class Pessoa
+    {
+
+        public string nome;
+        public int idade;
+        public string rua;
+        public string cidade;
+
+        //construtor
+
+        public Pessoa(string nome,int idade, string rua, string cidade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.rua = rua;
+            this.cidade = cidade;
+            Console.WriteLine("Salve");
+            
+        }
+
+        public void mostra()
+        {
+            Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos e moro na {rua} na cidade de {cidade}");
+        }
+    }
+}
---
+++ Fase2-Aula13Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Fase2_Aula13Classes;
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+
+Pessoa camila= new Pessoa("Camila", 19, "Rua rio Paranapanema","São Carlos");
+Pessoa conrado = new Pessoa("Conrado",27,"Rua epsicopal", "São carlos");
+Pessoa mike = new Pessoa("Mike", 22, "Rua Xl", "São carlos");
+Pessoa natan = new Pessoa("Natan", 23, "Rua wl", "São Carlos");
+
+
+List<Pessoa> pessoas= new List<Pessoa>();
+
+
+pessoas.Add(camila);
+pessoas.Add(conrado);
+pessoas.Add(mike);
+pessoas.Add(natan);
+
+for(int i = 0;i < pessoas.Count; i++)
+{
+
+    pessoas[i].mostra();
+    //Console.WriteLine(pessoas[i]);
+}
+
+
+camila.mostra();
+conrado.mostra();
+mike.mostra();
+natan.mostra();
+
+Console.ReadKey();
